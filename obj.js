d6e1e001e3f0c9e600efb570cf0ee806b99c37c1
Sammy <chinemeremsam91@gmail.com>
2021-04-07 10:46:09 +0100

    first commit

---
+++ obj.js
@@ -0,0 +1,11 @@
+// function Number(start,end,incr){
+// for (start=0; start<=end; start += incr)
+//     console.log (start);
+// }
+// Number(0,100,10)
+let start = 1;
+let end = 100;
+let add = 10;
+for(start=1; start <= end; start += add){
+    console.log(start);
+}
